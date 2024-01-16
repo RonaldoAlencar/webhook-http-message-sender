0d793fb6a78e7e411d1bee9f1e39fa8ff19a5544
Ronaldo Messias <ronaldo.messias@kabum.com.br>
2024-01-16 09:35:52 -0300

    feat: added generics on axios adapter

--- src/infra/adapters/rest/http/axios.adapter.ts
+++ src/infra/adapters/rest/http/axios.adapter.ts
@@ -2,7 +2,7 @@ import IHttpRequest from "../../../../business/common/adapters/http-request";
 import axios from "axios";
 
 export default class AxiosAdapter implements IHttpRequest {
-  async get(url: string, options?: any): Promise<any> {
+  async get<T>(url: string, options?: any): Promise<T> {
     return await axios.get(url, options);
   }
 }
